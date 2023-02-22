bebe13b2f4448983347aa0cf234aca1cec3e507d
KhalidSDET <khalid.guerziz@tekschool.us>
2023-02-22 16:46:16 -0500

    this is class practice

--- src/test/git/GitTest.java
+++ src/test/git/GitTest.java
@@ -5,4 +5,12 @@ public class GitTest {
 	public void test() {
 		System.out.println("this is just a test");
 	}
+	
+	public void testTow() {
+		System.out.println("this is class practice");
+	}
+	
+	
+	
+	
 }
