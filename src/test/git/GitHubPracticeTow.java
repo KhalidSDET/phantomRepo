b9579a9d73c0ab725d0a8df39c9d0febe0522351
KhalidSDET <khalid.guerziz@tekschool.us>
2023-02-27 15:09:41 -0500

    this is new change

--- src/test/git/GitHubPracticeTow.java
+++ src/test/git/GitHubPracticeTow.java
@@ -6,7 +6,7 @@ public class GitHubPracticeTow {
 		return a*b;
 	}
 	
-public int test(int x, int y, String message) {
+	public int test(int x, int y, String message) {
 	System.out.println(message);
 	int z = x+y;
 	return z;
